3bdd49eb2fce627daf42a952c96cf6e90fc4a6d6
Jessie Jalca <jessiejalca@gmail.com>
2022-12-20 15:53:24 -0500

    Use basic component features

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,20 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
+const App = () => {
+  // Any JS code can run in component functions
+  console.log("Hello from component");
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+
+  // We can also render dynamic content inside components
+  // Use { } to embed content in the HTML
+  return (
+    <div>
+      <p>Hello world, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  );
+};
 
 export default App;
