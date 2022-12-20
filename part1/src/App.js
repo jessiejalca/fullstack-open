aec3b3fca11ff54640d8be86e09455b53fb0fe87
Jessie Jalca <jessiejalca@gmail.com>
2022-12-20 16:10:15 -0500

    Use props to show dynamic content

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,18 +1,28 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+// Usually there's a root component called App at the top of the component tree of a given application
 const App = () => {
   // Any JS code can run in component functions
   console.log("Hello from component");
-  const now = new Date();
-  const a = 10;
-  const b = 20;
+  const name = "Peter";
+  const age = 21;
 
   // We can also render dynamic content inside components
-  // Use { } to embed content in the HTML
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <h1>Greetings</h1>
+      {/* Components can be used multiple times & combined in other components */}
+      <Hello name={name} age={age} />
+      {/* Use { } to embed content in the JSX/HTML */}
+      <Hello name="Maya" age={26 + 10} />
     </div>
   );
 };
