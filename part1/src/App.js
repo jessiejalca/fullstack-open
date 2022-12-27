1eec255bba77036c273693ab9acaf4d70127e0bc
Jessie Jalca <jessiejalca@gmail.com>
2022-12-27 15:23:15 -0500

    Use props & JS results to customize a simple component

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,7 +1,26 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-);
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  console.log("Hello from component");
+
+  const name = "Peter";
+  const age = 18;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  );
+};
 
 export default App;
