4e4ac89f165254805bb5691772c0648bcf164757
Jessie Jalca <jessiejalca@gmail.com>
2022-12-27 14:52:39 -0500

    Start basic React app for part 1 from the beginning

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,31 +1,7 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  );
-};
-
-// Usually there's a root component called App at the top of the component tree of a given application
-const App = () => {
-  // Any JS code can run in component functions
-  console.log("Hello from component");
-  const name = "Peter";
-  const age = 21;
-
-  // We can also render dynamic content inside components
-  return (
-    // We can use fragments to avoid "extra" div elements in the DOM
-    <>
-      <h1>Greetings</h1>
-      {/* Components can be used multiple times & combined in other components */}
-      <Hello name={name} age={age} />
-      {/* Use { } to embed content in the JSX/HTML */}
-      <Hello name="Maya" age={26 + 10} />
-    </>
-  );
-};
+const App = () => (
+  <div>
+    <p>Hello world</p>
+  </div>
+);
 
 export default App;
