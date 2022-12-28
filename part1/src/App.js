c1cdee515b5b58c66732bceed0a7fa21a11bc47a
Jessie Jalca <jessiejalca@gmail.com>
2022-12-27 23:15:29 -0500

    Revert "Start basic React app for part 1 from the beginning"
    This reverts commit 4e4ac89f165254805bb5691772c0648bcf164757.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,7 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  );
-};
-
-const App = () => {
-  console.log("Hello from component");
-
-  const name = "Peter";
-  const age = 18;
-
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <p>Hello world</p>
+  </div>
+);
 
 export default App;
